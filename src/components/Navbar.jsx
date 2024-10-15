30e98d4ea4dbd910804d47aa730e48008ebb7551
Muhammad <talha18513@gmail.com>
2024-10-15 08:35:03 +0500

    I added text to the file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link, NavLink } from 'react-router-dom'
 
 const TopNavbar = () => {
 
-    const [toggleNavbar, seToggleNavbar] = useState(false)
+    const [toggleNavbar, setToggleNavbar] = useState(false)
 
     return (
         <>
@@ -16,25 +16,30 @@ const TopNavbar = () => {
                         <HiMiniBars3 className='text-2xl cursor-pointer' />
                         <IoIosSearch className='text-2xl cursor-pointer' />
 
-                        <Link onClick={() => seToggleNavbar(!toggleNavbar)} to={!toggleNavbar ? "servey" : "/"} className=' py-2 transition-all duration-300 font-medium hover:underline'>{!toggleNavbar ? "Survey" : "Home"}</Link>
 
                     </div>
-                    <div className="">
+                    <Link to={"/"}>
                         <img src="/public/Images/Home/logo.png" className='max-w-[97px] w-full' alt="" />
-                    </div>
+                    </Link>
                     <div className="md:flex hidden items-center max-w-[14rem] w-full gap-3">
                         <Link to={"login"}>
-                            <button onClick={() => seToggleNavbar(true)} className='hover:bg-black border-black px-5 py-2 transition-all duration-300 text-black bg-white border-2 hover:text-white font-medium'>Sign In</button>
+                            <button onClick={() => setToggleNavbar(true)} className='hover:bg-black border-black px-5 py-2 transition-all duration-300 text-black bg-white border-2 hover:text-white font-medium'>Sign In</button>
                         </Link>
                         <Link to={"register"}>
-                            <button onClick={() => seToggleNavbar(true)} className='bg-black border-black px-5 py-2 transition-all duration-300 hover:text-black hover:bg-white border-2 text-white'>Register</button>
+                            <button onClick={() => setToggleNavbar(true)} className='bg-black border-black px-5 py-2 transition-all duration-300 hover:text-black hover:bg-white border-2 text-white'>Register</button>
                         </Link>
                     </div>
                 </div>
 
+                <div className=" flex justify-center items-center mb-4">
+                    <div className="border rounded-full flex justify-center overflow-hidden  items-center">
+                        <Link to={"/"} onClick={() => setToggleNavbar(false)} className=' py-3 block px-5 pl-7 transition-all duration-500 font-medium hover:bg-zinc-50'>Home</Link>
+                        <Link to={"/servey"} onClick={() => setToggleNavbar(false)} className=' py-3 block px-5 pr-7 transition-all duration-500 font-medium hover:bg-zinc-50'>Survey</Link>
+                    </div>
+                </div>
                 {
                     !toggleNavbar && (
-                        <div className="border-y md:block hidden fastFadeIn">
+                        <div className="border-b md:block hidden fastFadeIn">
                             <div className="md:px-10 px-5 py-1.5  mx-auto max-w-7xl w-full">
                                 <ul className='flex items-center justify-center gap-5 font-medium'>
                                     <li> <NavLink className={"navUnderLine"} to={"/"}> Home</NavLink></li>
--- src/pages/Servey.jsx
+++ src/pages/Servey.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Servey = () => {
     return (
         <>
-            <div className="fastFadeIn md:text-5xl text-2xl  font-bold flex items-center justify-center h-screen">Servey</div>
+
+            <div className="fastFadeIn">
+                <h2 className='text-center md:text-3xl text-2xl font-bold md:mt-12 mt-8'>Features Content</h2>
+
+                <div className="max-w-7xl mx-auto w-full p-5">
+
+                
+                    
+                </div>
+                
+            </div>
+
         </>
     )
 }
