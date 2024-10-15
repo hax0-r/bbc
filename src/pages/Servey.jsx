f08b0b1ca1bafa0f632befe5929a9cbb73f77e38
Muhammad <talha18513@gmail.com>
2024-10-15 11:21:28 +0500

    I added text to the file

--- src/pages/Servey.jsx
+++ src/pages/Servey.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import Votes from '../components/Votes'
 
 const Servey = () => {
     return (
         <>
 
             <div className="fastFadeIn">
-                <h2 className='text-center md:text-3xl text-2xl font-bold md:mt-12 mt-8'>Features Content</h2>
+                <h2 className='text-center md:text-3xl text-2xl font-bold md:mt-12 mt-8 '>Features Content</h2>
 
                 <div className="max-w-7xl mx-auto w-full p-5">
 
-                
-                    
+                    <div className="max-w-7xl w-full p-5 mx-auto grid grid-cols-3 gap-5">
+
+                        <Votes />
+                        <Votes />
+                        <Votes />
+                        <Votes />
+                        <Votes />
+                        <Votes />
+
+                    </div>
+
                 </div>
-                
+
             </div>
 
         </>
